831689f9be42c69b5c234666a7c650c870c00075
DeElfos <59063880+DeElfos@users.noreply.github.com>
2021-03-01 12:21:35 -0300

    teste

--- Libs/BotConfig.js
+++ Libs/BotConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     'BotName': 'BotJs',
     'BotConfidence': { hight: 0.60, medium: 0.51, low: 0.25 },
-    'DB': 'mongodb://mongo-chatbotnodejs:27017/BotJs',
+    'DB': 'mongodb+srv://shawdom:Marcos@andre1@cluster0.papp5.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     'Language': 'en'
 };
--- seed/index.js
+++ seed/index.js
@@ -8,13 +8,13 @@ var synonymModel = require('../models/synonyms');
 mongoose
     .connect(config.DB, { useNewUrlParser: true })
     .then(async () => {
-        console.log('Running seed')
+        console.log('Semear em progresso')
         let intents = require('./intents')
         let synonym = require('./synonyms')
 
-        Promise.all(intents.forEach(async int => await new intentsModels(int).save().then(() => console.log('saved intent')).catch(console.error)))
+        Promise.all(intents.forEach(async int => await new intentsModels(int).save().then(() => console.log('Intents Salvas')).catch(console.error)))
 
-        Promise.all(synonym.forEach(async syn => await new synonymModel(syn).save().then(() => console.log('saved synonym')).catch(console.error)))
+        Promise.all(synonym.forEach(async syn => await new synonymModel(syn).save().then(() => console.log('Synonym Salvas')).catch(console.error)))
         
         console.log('done')
     }).catch(err => console.log(err)); 
